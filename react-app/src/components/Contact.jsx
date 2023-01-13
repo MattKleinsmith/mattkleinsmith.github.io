951c3e071e0c2ae99d60381ed07cbe5aac7d1739
Matt Kleinsmith <8968171+MattKleinsmith@users.noreply.github.com>
2023-01-13 16:38:47 -0600

    Add discord icon

--- react-app/src/components/Contact.jsx
+++ react-app/src/components/Contact.jsx
@@ -83,44 +83,30 @@ const Contact = ({ classicHeader, darkTheme }) => {
         <div className="row gy-5">
           {/* contact details */}
           <div className="col-md-4 col-xl-3 order-1 order-md-0 text-center text-md-start">
-            <h2
-              className={
-                "mb-3 text-5 text-uppercase " + (darkTheme ? "text-white" : "")
-              }
-            >
-              Address
-            </h2>
-            <p className={"text-3 mb-4 " + (darkTheme ? "text-light" : "")}>
-              4th Floor, Plot No.22,
-              <br />
-              145 Murphy Canyon Rd.
-              <br />
-              San Diego CA 2028
+            <p className={"text-3 mb-1 " + (darkTheme ? "text-light" : "")}>
+              <span className="text-primary text-4 me-2">
+                <i className="fas fa-envelope" />
+              </span>
+              <a href="mailto:mwksmith@gmail.com">mwksmith@gmail.com</a>
             </p>
             <p className={"text-3 mb-1 " + (darkTheme ? "text-light" : "")}>
               <span className="text-primary text-4 me-2">
                 <i className="fas fa-phone" />
               </span>
-              (060) 444 434 444
-            </p>
-            <p className={"text-3 mb-1 " + (darkTheme ? "text-light" : "")}>
-              <span className="text-primary text-4 me-2">
-                <i className="fas fa-fax" />
-              </span>
-              (060) 555 545 555
+              925-457-2959
             </p>
             <p className={"text-3 mb-4 " + (darkTheme ? "text-light" : "")}>
-              <span className="text-primary text-4 me-2">
-                <i className="fas fa-envelope" />
-              </span>
-              chat@simone.com
+              <div className="flex-center">
+                <i className="discord" />
+                <div>MattK#9804</div>
+              </div>
             </p>
             <h2
               className={
                 "mb-3 text-5 text-uppercase " + (darkTheme ? "text-white" : "")
               }
             >
-              Follow Me
+              Social
             </h2>
             <ul
               className={
@@ -128,54 +114,21 @@ const Contact = ({ classicHeader, darkTheme }) => {
                 (darkTheme ? "social-icons-muted" : "")
               }
             >
-              <li className="social-icons-dribbble">
-                <Tooltip text="Dribbble" placement="top">
+              <li className="social-icons-github">
+                <Tooltip text="LinkedIn" placement="top">
                   <a
-                    href="http://www.dribbble.com/harnishdesign/"
+                    href="https://www.linkedin.com/in/matthewkleinsmith/"
                     target="_blank"
                     rel="noopener noreferrer"
                   >
-                    <i className="fab fa-dribbble" />
-                  </a>
-                </Tooltip>
-              </li>
-              <li className="social-icons-twitter">
-                <Tooltip text="Twitter" placement="top">
-                  <a
-                    href="https://twitter.com/harnishdesign/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <i className="fab fa-twitter" />
-                  </a>
-                </Tooltip>
-              </li>
-              <li className="social-icons-facebook">
-                <Tooltip text="Facebook" placement="top">
-                  <a
-                    href="http://www.facebook.com/harnishdesign/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <i className="fab fa-facebook-f" />
-                  </a>
-                </Tooltip>
-              </li>
-              <li className="social-icons-google">
-                <Tooltip text="Google" placement="top">
-                  <a
-                    href="http://www.google.com/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <i className="fab fa-google" />
+                    <i className="fab fa-linkedin" />
                   </a>
                 </Tooltip>
               </li>
               <li className="social-icons-github">
                 <Tooltip text="Github" placement="top">
                   <a
-                    href="http://www.github.com/"
+                    href="http://www.github.com/mattkleinsmith"
                     target="_blank"
                     rel="noopener noreferrer"
                   >
